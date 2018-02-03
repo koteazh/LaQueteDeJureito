b2cb0c3e12c4b30e7516f86821bb1096b06c3307
tbrunel <titi.brunel@gmail.com>
2018-02-03 14:02:57 +0100

    Armors and weapons stats
    Addition of the stats of all armors and weapons type / quality

---
+++ Assets/Equipment/Armor.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Armor : MonoBehaviour {
+	public enum ArmorType { Light, Medium, Heavy }
+	public enum ArmorQuality { Poor, Common, Excellent }
+
+	public ArmorType type;
+	public ArmorQuality quality;
+	public int physicalProtection { get; private set; }
+	public int magicalProtection { get; private set; }
+	public int congestion { get; private set; }
+	public string statRequirementName  { get; private set; }
+	public int statRequirementValue  { get; private set; }
+
+	void Start()
+	{
+		switch (type)
+		{
+			case ArmorType.Light:
+				switch (quality)
+				{
+					case ArmorQuality.Poor:
+						physicalProtection = 1;
+						magicalProtection = 3;
+						congestion = 0;
+						statRequirementName = "Resistance";
+						statRequirementValue = 15;
+						break;
+
+					case ArmorQuality.Common:
+						physicalProtection = 2;
+						magicalProtection = 4;
+						congestion = 0;
+						statRequirementName = "Resistance";
+						statRequirementValue = 20;
+						break;
+
+					case ArmorQuality.Excellent:
+						physicalProtection = 3;
+						magicalProtection = 5;
+						congestion = 1;
+						statRequirementName = "Resistance";
+						statRequirementValue = 25;
+						break;
+				}
+				break;
+
+			case ArmorType.Medium:
+				switch (quality)
+				{
+					case ArmorQuality.Poor:
+						physicalProtection = 2;
+						magicalProtection = 2;
+						congestion = 1;
+						statRequirementName = "Agility";
+						statRequirementValue = 20;
+						break;
+
+					case ArmorQuality.Common:
+						physicalProtection = 3;
+						magicalProtection = 3;
+						congestion = 1;
+						statRequirementName = "Agility";
+						statRequirementValue = 25;
+						break;
+
+					case ArmorQuality.Excellent:
+						physicalProtection = 5;
+						magicalProtection = 5;
+						congestion = 2;
+						statRequirementName = "Agility";
+						statRequirementValue = 30;
+						break;
+				}
+				break;
+
+			case ArmorType.Heavy:
+				switch (quality)
+				{
+					case ArmorQuality.Poor:
+						physicalProtection = 3;
+						magicalProtection = 2;
+						congestion = 3;
+						statRequirementName = "Defense";
+						statRequirementValue = 10;
+						break;
+
+					case ArmorQuality.Common:
+						physicalProtection = 5;
+						magicalProtection = 3;
+						congestion = 3;
+						statRequirementName = "Defense";
+						statRequirementValue = 15;
+						break;
+
+					case ArmorQuality.Excellent:
+						physicalProtection = 8;
+						magicalProtection = 5;
+						congestion = 4;
+						statRequirementName = "Defense";
+						statRequirementValue = 20;
+						break;
+				}
+				break;
+		}
+	}
+}
+
+
---
+++ Assets/Equipment/Weapon.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+	public enum WeaponType { Sword, Dagger, Bow, Magic }
+	public enum WeaponQuality { Poor, Common, Excellent }
+
+	public WeaponType type;
+	public WeaponQuality quality;
+	public int damage { get; private set; }
+	public int precision { get; private set; }
+	public string statRequirementName  { get; private set; }
+	public int statRequirementValue  { get; private set; }
+
+	void Start()
+	{
+		switch (type)
+		{
+		case WeaponType.Sword:
+			switch (quality)
+			{
+			case WeaponQuality.Poor:
+				damage = 5;
+				precision = 80;
+				statRequirementName = "Strength";
+				statRequirementValue = 15;
+				break;
+
+			case WeaponQuality.Common:
+				damage = 7;
+				precision = 77;
+				statRequirementName = "Strength";
+				statRequirementValue = 20;
+				break;
+
+			case WeaponQuality.Excellent:
+				damage = 10;
+				precision = 73;
+				statRequirementName = "Strength";
+				statRequirementValue = 25;
+				break;
+			}
+			break;
+
+		case WeaponType.Dagger:
+			switch (quality)
+			{
+			case WeaponQuality.Poor:
+				damage = 3;
+				precision = 90;
+				statRequirementName = "Dexterity";
+				statRequirementValue = 20;
+				break;
+
+			case WeaponQuality.Common:
+				damage = 6;
+				precision = 87;
+				statRequirementName = "Dexterity";
+				statRequirementValue = 25;
+				break;
+
+			case WeaponQuality.Excellent:
+				damage = 9;
+				precision = 85;
+				statRequirementName = "Dexterity";
+				statRequirementValue = 30;
+				break;
+			}
+			break;
+
+		case WeaponType.Bow:
+			switch (quality)
+			{
+			case WeaponQuality.Poor:
+				damage = 4;
+				precision = 85;
+				statRequirementName = "Dexterity";
+				statRequirementValue = 25;
+				break;
+
+			case WeaponQuality.Common:
+				damage = 8;
+				precision = 85;
+				statRequirementName = "Dexterity";
+				statRequirementValue = 30;
+				break;
+
+			case WeaponQuality.Excellent:
+				damage = 12;
+				precision = 85;
+				statRequirementName = "Dexterity";
+				statRequirementValue = 35;
+				break;
+			}
+			break;
+
+		case WeaponType.Magic:
+			switch (quality)
+			{
+			case WeaponQuality.Poor:
+				damage = 5;
+				precision = 80;
+				statRequirementName = "Strength";
+				statRequirementValue = 20;
+				break;
+
+			case WeaponQuality.Common:
+				damage = 7;
+				precision = 77;
+				statRequirementName = "Strength";
+				statRequirementValue = 25;
+				break;
+
+			case WeaponQuality.Excellent:
+				damage = 10;
+				precision = 73;
+				statRequirementName = "Strength";
+				statRequirementValue = 30;
+				break;
+			}
+			break;
+		}
+	}
+}
